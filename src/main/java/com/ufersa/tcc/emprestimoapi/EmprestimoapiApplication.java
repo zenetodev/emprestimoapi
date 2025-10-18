6c2feca6b1356661e113060733befe89cba24538
zenetiin <jose.neto56334@alunos.ufersa.edu.br>
2025-10-18 12:09:37 -0300

    emprestimoapi

---
+++ src/main/java/com/ufersa/tcc/emprestimoapi/EmprestimoapiApplication.java
@@ -0,0 +1,13 @@
+package com.ufersa.tcc.emprestimoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestimoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestimoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufersa/tcc/emprestimoapi/EmprestimoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufersa.tcc.emprestimoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestimoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
